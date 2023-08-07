1e511b4a88d7c40733e244dc78d355255e18afff
serkantek179 <141683939+serkantek179@users.noreply.github.com>
2023-08-08 01:27:19 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x62c6ac88e285eb8e7e85a2b2b0961b2cb3f2f256";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
